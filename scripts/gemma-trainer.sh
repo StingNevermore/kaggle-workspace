83170f76cdf10dc0c4307fe636f20ec0cf628b6d
Victor.Sun <406504325@qq.com>
2024-11-14 19:57:45 +0800

    1024

--- scripts/gemma-trainer.sh
+++ scripts/gemma-trainer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-identifier="gemma-2-9b-it"
+identifier="gemma-2-9b-it-1024"
 fs_dir="/root/autodl-fs"
 data_disk_dir="/root/autodl-tmp"
 model_name_or_path="${fs_dir}/pretrained/gemma-2-9b-it"
 tokenizer_name_or_path="${fs_dir}/pretrained/gemma-2-9b-it"
-dataset_dir="${fs_dir}/datasets/llm-classification-full"
+dataset_dir="${fs_dir}/datasets/llm-classification"
 data_dir="${data_disk_dir}/train-${identifier}"
 logs_dir="/root/tf-logs"
 scripts_dir=$(dirname $(readlink -f "$0"))
@@ -18,7 +18,7 @@ accelerate launch --config_file ${scripts_dir}/default_deepseepd_config.yaml ${s
     --tokenizer_name_or_path "${tokenizer_name_or_path}" \
     --dataset_dir "${dataset_dir}" \
     --num_classes 3 \
-    --max_seq_length 512 \
+    --max_seq_length 1024 \
     --per_device_train_batch_size ${batch_size} \
     --per_device_eval_batch_size ${batch_size} \
     --learning_rate 4e-4 \
--- scripts/llama-trainer.sh
+++ scripts/llama-trainer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-identifier="llama-3-8b-instruct"
+identifier="llama-3-8b-instruct-1024"
 fs_dir="/root/autodl-fs"
 data_disk_dir="/root/autodl-tmp"
 model_name_or_path="${fs_dir}/pretrained/llama-3-8b-instruct"
 tokenizer_name_or_path="${fs_dir}/pretrained/llama-3-8b-instruct"
-dataset_dir="${fs_dir}/datasets/llm-classification-full"
+dataset_dir="${fs_dir}/datasets/llm-classification"
 data_dir="${data_disk_dir}/train-${identifier}"
 logs_dir="/root/tf-logs"
 scripts_dir=$(dirname $(readlink -f "$0"))
@@ -18,7 +18,7 @@ accelerate launch --config_file ${scripts_dir}/default_deepseepd_config.yaml ${s
     --tokenizer_name_or_path "${tokenizer_name_or_path}" \
     --dataset_dir "${dataset_dir}" \
     --num_classes 3 \
-    --max_seq_length 512 \
+    --max_seq_length 1024 \
     --per_device_train_batch_size ${batch_size} \
     --per_device_eval_batch_size ${batch_size} \
     --learning_rate 4e-4 \
@@ -28,4 +28,4 @@ accelerate launch --config_file ${scripts_dir}/default_deepseepd_config.yaml ${s
     --identifier "${identifier}" \
     --eval_steps 0.2 \
     --logging_steps 50 \
-    --warmup_steps 100 
+    --warmup_steps 100
